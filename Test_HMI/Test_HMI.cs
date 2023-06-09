f941a2ef2d5a945c2fa27f78a18bf44a1f93e32b
Brummer98 <437019@student.fontys.nl>
2023-06-09 16:46:07 +0200

    Added testing
    Functional

--- Test_HMI/Test_HMI.cs
+++ Test_HMI/Test_HMI.cs
@@ -1,19 +1,59 @@
-namespace Test_HMI
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Data___Layer;
+
+namespace Data___Layer.Tests
 {
-    [TestClass]
+    [TestClass()]
     public class Test_HMI
     {
-        // Connect to DB to test data
-
-        public Test_HMI() 
-        { 
-            // Test
-        }
-
-        [TestMethod]
-        public void Test_CreateCRUD()
+        [TestMethod()]
+        public void getSingleTagTest()
         {
             // Arrange
+            List<TagDTOData> singleTagList = new List<TagDTOData>();
+            Data___Layer.TagCollectionData testDataCollection = new Data___Layer.TagCollectionData();
+
+            // Act
+            singleTagList = testDataCollection.getSingleTag(4);
+
+            // Assert
+            Assert.IsTrue(singleTagList.Count == 1);
+        }
+
+        [TestMethod()]
+        public void GetAllTagsTest()
+        {
+            // Arrange
+            List<TagDTOData> singleTagList = new List<TagDTOData>();
+            Data___Layer.TagCollectionData testDataCollection = new Data___Layer.TagCollectionData();
+
+            // Act
+            singleTagList = testDataCollection.GetAllTags();
+
+            // Assert
+            Assert.IsTrue(singleTagList.Count > 0);
+        }
+
+        [TestMethod()]
+        public void GetAllTagsHMISignalsTest()
+        {
+            // Arrange
+            List<TagDTOData> singleTagList = new List<TagDTOData>();
+            Data___Layer.TagCollectionData testDataCollection = new Data___Layer.TagCollectionData();
+
+            // Act
+            singleTagList = testDataCollection.GetAllTagsHMISignals();
+
+            // Assert
+            Assert.IsTrue(singleTagList.Count > 0);
         }
     }
 }
